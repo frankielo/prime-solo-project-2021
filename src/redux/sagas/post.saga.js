87e710224cba7ab78df5ab2874e88bbec2eca73c
Francisco Lopez <frankie.b.lopez@gmail.com>
2022-01-01 12:48:46 -0600

    fixed posts saga to manage the network request related to post

---
+++ src/redux/sagas/post.saga.js
@@ -0,0 +1,72 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* getPost() {
+
+  try {
+
+    const response = yield  axios.get("/api/posts/user")   
+    
+    yield put({ type: 'SET_POSTS', payload : response.data });
+
+  } catch (error) {
+    console.log('Error with get post saga:', error);
+
+  }
+}
+
+
+function* getPublishedPost() {
+
+  try {
+
+    const response = yield  axios.get("/api/posts")   
+    
+    yield put({ type: 'SET_PUBLISHED_POSTS', payload : response.data });
+    // console.log(response.data,'coming from saga')
+  } catch (error) {
+    console.log('Error with get published post saga:', error);
+
+  }
+}
+
+
+
+function* getAllPost() {
+
+  try {
+
+    const response = yield  axios.get("/api/posts/all")   
+    yield put({ type: 'SET_ALL_POSTS', payload : response.data });
+
+  } catch (error) {
+    console.log('Error with get all post saga:', error);
+
+  }
+}
+
+function* togglePublish(action) {
+
+  try {
+
+    yield  axios.put(`/api/posts/${action.payload.rowId}`,{status : action.payload.status})  
+    yield put({ type: 'FETCH_ALL_POSTS'});
+
+  } catch (error) {
+    console.log('Error with get all post saga:', error);
+
+  }
+}
+
+
+
+
+
+function* postSaga() {
+  yield takeLatest('FETCH_POST', getPost);
+  yield takeLatest('FETCH_PUBLISHED_POSTS', getPublishedPost);
+  yield takeLatest('FETCH_ALL_POSTS', getAllPost);
+  yield takeLatest('TOGGLE_BLOG_ACTIVATION', togglePublish);
+}
+
+export default postSaga;
