6fd46fc7d7be40262e72a8bf04e4529a70269929
Francisco Lopez <frankie.b.lopez@gmail.com>
2022-01-01 12:29:47 -0600

    finished working on the backend

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const postRouter = require('./routes/posts.router');
+const categoryRouter = require('./routes/categories.router');
+
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +26,8 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/posts', postRouter);
+app.use('/api/category', categoryRouter);
 
 // Serve static files
 app.use(express.static('build'));
