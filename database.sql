596821d84183da5f6f20968ba7eae5a70a2c4a7d
Francisco Lopez <frankie.b.lopez@gmail.com>
2022-01-01 12:12:36 -0600

    created database

--- database.sql
+++ database.sql
@@ -1,58 +1,43 @@
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL,
-	"user_role" varchar(255) NOT NULL DEFAULT 'regular',
-	"user_image" varchar(255),
-	"user_status" varchar(255) NOT NULL DEFAULT 'active'
-);
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    "post_title" varchar(255) NOT NULL,
-    "post_author" varchar(255),
-    "post_date" DATE NOT NULL,
-    "post_image" TEXT,
-    "post_content" varchar(1000) NOT NULL,
-    "post_status" varchar(255) NOT NULL DEFAULT 'draft',
-    "post_userid" integer REFERENCES "users",
-)
-CREATE TABLE categories (
-    id SERIAL PRIMARY KEY,
-    "cat_title" varchar(255) NOT NULL UNIQUE
-)
-CREATE TABLE comments (
-    id SERIAL PRIMARY KEY,
-    "comment_author" varchar(255) NOT NULL,
-    "comment_email" varchar(255) NOT NULL,
-    "comment_content" varchar(255) NOT NULL,
-    "comment_date" DATE NOT NULL,
-    "comment_status" varchar(255) NOT NULL DEFAULT 'unapproved',
-    "comment_postid" integer REFERENCES "posts" NOT NULL,
-    "comment_userid" integer REFERENCES "users"
-)
-CREATE TABLE categories_posts (
-    id SERIAL PRIMARY KEY,
-    "post_id" integer REFERENCES "posts" NOT NULL,
-    "category_id" integer REFERENCES "categories" NOT NULL,
-)
-
-INSERT INTO "users" ("username", "user_email", "user_password", "user_firstname", "user_lastname",
- "user_role", "user_image")
-VALUES ('Frankie', 'any@gmail.com', 'mypassword', 'myfirstname', 'mylastname', "admin", NULL)
-
-
-INSERT INTO "posts" ("post_title", "post_author", "post_date", "post_image", "post_content", "post_status",
-"post_userid")
-VALUES ('My First Blog', 'Frankie', '2021-15-12', NULL, 'The content of this post', '', 1)
 
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+	"user_role" varchar(255) NOT NULL DEFAULT 'regular',
+	"user_image" varchar(1000)
+);
 
 
--- CREATE TABLE "user" (
---     "id" SERIAL PRIMARY KEY,
---     "username" VARCHAR (80) UNIQUE NOT NULL,
---     "password" VARCHAR (1000) NOT NULL
--- );
+CREATE TABLE posts (
+	id SERIAL PRIMARY KEY,
+	"post_title" varchar(255) NOT NULL,
+	"post_author" varchar(255),
+	"post_date" DATE NOT NULL DEFAULT NOW(),
+	"post_image_url" varchar(1000),
+	"post_image_name" varchar(255),
+	"post_content" varchar(1000) NOT NULL,
+	"post_status" varchar(255) NOT NULL DEFAULT 'draft',
+	"post_userid" bigint NOT NULL,
+	FOREIGN KEY ("post_author") REFERENCES "user"(username) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY ("post_userid") REFERENCES "user"(id) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
+
+CREATE TABLE categories (
+	id SERIAL PRIMARY KEY,
+	"cat_title" varchar(255) NOT NULL UNIQUE
+)
+
+CREATE TABLE categories_posts (
+	id SERIAL PRIMARY KEY,
+	"post_id"  bigint NOT NULL,
+	"category_id"  bigint NOT NULL,
+	FOREIGN KEY ("post_id") REFERENCES "posts"(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY ("category_id") REFERENCES "categories"(id) ON DELETE CASCADE ON UPDATE CASCADE
+	)
+
+
