690b04c2c00ff734a3824328ab75ecd3ae09fe4c
Francisco Lopez <frankie.b.lopez@gmail.com>
2022-01-01 12:26:21 -0600

    added authorization middleware for admin

--- server/modules/authentication-middleware.js
+++ server/modules/authentication-middleware.js
@@ -10,4 +10,17 @@ const rejectUnauthenticated = (req, res, next) => {
   }
 };
 
-module.exports = { rejectUnauthenticated };
+const rejectUnauthorized = (req, res, next) => {
+  // check if logged in
+  if (req.user.user_role === 'admin') {
+    // They were authorized! User may do the next thing
+    next();
+  } else {
+    // failure best handled on the server. do redirect here.
+    res.sendStatus(403);
+  }
+};
+
+
+
+module.exports = { rejectUnauthenticated, rejectUnauthorized };
