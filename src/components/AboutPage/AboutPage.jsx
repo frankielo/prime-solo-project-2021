c51dd7f60355731bad6d8cbeb8a0d75423208a7c
Francisco Lopez <frankie.b.lopez@gmail.com>
2022-01-06 06:46:24 -0600

    updated aboutpage

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -7,11 +10,27 @@ import React from 'react';
 
 function AboutPage() {
   return (
-    <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
-      </div>
-    </div>
+    <Grid container direction="column" alignContent="center" style={{marginTop: "3rem"}} spacing={2}>
+      <Grid item>
+        <Typography variant="h4">Hello World</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="body1">My name is Frankie Lopez and I am a Full Stack Web Developer.</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="h4">Technologies</Typography>
+        <ul>
+          <li>Node</li>
+          <li>Express</li>
+          <li>React w/ Hooks, Redux, Saga</li>
+          <li>Passport w/ Local Authentication</li>
+          <li>Postgresql</li>
+          <li>Heroku</li>
+          <li>Moment.js</li>
+          <li>MUI, MUI Icons</li>
+        </ul>
+      </Grid>
+    </Grid>
   );
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; DaBlogz</footer>;
+  return <footer>&copy; Tekblogz</footer>;
 }
 
 export default Footer;
